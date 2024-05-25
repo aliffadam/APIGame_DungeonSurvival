5102c70dcb7f215c463c1374aa7d2cacdd47e60a
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-05-25 15:06:33 +0800

    Create next_move.js

---
+++ next_move.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const Next_Action_Router = express.Router();
+module.exports = Next_Action_Router;
+
+let client = require(`./database.js`)
+
+Next_Action_Router.get('/next_action', async (req, res) => {
+
+    if(req.body.action == "attack") {
+
+        let player = await client.db('ds_db').collection('stats').findOne(
+            playerId = req.body.name
+        )
+
+        console.log(player)
+        //attack action cukup tak?
+        //enemy health utk tolak
+
+//        if()
+
+    } else if(req.body.action == "evade") {
+
+    } else if(req.body.action == "defend") {
+
+    } else {
+        res.send("Invalid Action")
+    }
+
+})
