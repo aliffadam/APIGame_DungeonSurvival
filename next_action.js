eea2272a94ac53691025d387ccb6da80f8ea9a26
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-05-29 15:26:34 +0800

    fix bugs

--- next_action.js
+++ next_action.js
@@ -23,88 +23,144 @@ Next_Action_Router.get('/next_action', async (req, res) => {    // player give t
 
     if(req.body.action == "attack") {       //if player choose to attack
 
-        console.log(player)
+        //console.log(player)
 
         if(player.attack_action > 0) {      //attack action available?
-            res.send("attack")
 
             let after_player_action = await client.db('ds_db').collection('stats').updateOne(   //damage to enemy and consume attack action
                 {playerId : player.playerId},
                 { $inc: {enemy_current_health: -2, attack_action: -1} }
             )
 
-            //TODO
-            //check if enemy health <= 0 //refer to 2 as it is not 
+            //check if enemy health <= 0 //refer to 2 as it must not be less than zero
             if(player.enemy_current_health <= 2) {
+
+                let enemy_list = await client.db('ds_db').collection('almanac').find().toArray()
                 
-                //if yes, then update it with a new randomised enemy
-                
+                //making a random index to choose in almanac
+                let randomEnemyIndex = Math.floor(Math.random() * enemy_list.length)
+
+                //this is the enemy chosen at random
+                let chosenEnemy = enemy_list[randomEnemyIndex]
+
+                //making a random index to choose enemy skill
+                let randomEnemySkillIndex = Math.floor(Math.random() * chosenEnemy.skill.length)
+
+                //this is the skill of the enemy chosen at random
+                let chosenEnemySkill = chosenEnemy.skill[randomEnemySkillIndex]
+
+                //update it with a new randomised enemy
+                let new_enemy = await client.db('ds_db').collection('stats').updateOne(
+                    {playerId : player.playerId},
+                    { $set: 
+                        { 
+                            current_enemy: chosenEnemy.enemy,
+                            enemy_current_health: chosenEnemy.base_health,
+                            enemy_next_move: chosenEnemySkill
+                        }
+                    }
+                )
+
+                res.send(`The ${player.current_enemy} died\nA ${chosenEnemy.enemy} appeared!`)
 
                 //don't continue code (use return)
                 return
             }
-            console.log("after player action: player")
-            console.log(after_player_action)
 
+            let enemy_action_damage = player.enemy_next_move.damage  //get enemy_next_move damage from stats(player's stats)
 
-            //console.log(after_player_action)
-            //console.log(player.enemy_next_move)
-
-            let enemy_next_action = player.enemy_next_move  //get enemy_next_move from stats(player's stats)
-            let enemy_almanac = await client.db('ds_db').collection('almanac').findOne({skill:{$elemMatch:{attack_name:enemy_next_action}}})    //finding info from almanac
-
-            let enemy_skill = enemy_almanac.skill.find(skill => skill.attack_name === enemy_next_action);   //get the json corresponding to its attack_name
-
-            console.log(enemy_skill.damage);
-            console.log(enemy_skill);
+            console.log(player.enemy_next_move);
+            //console.log(enemy_action_damage);
 
             let after_enemy_action = await client.db('ds_db').collection('stats').updateOne(    //enemy damage the player based on its next_attack
                 {playerId : player.playerId},
-                { $inc: {health_pts: (-1 * enemy_skill.damage)} }
+                { $inc: {health_pts: (-1 * enemy_action_damage)} }
             )
 
-            //TODO
             //randomise enemy next action
+            let enemy_current = await client.db('ds_db').collection('almanac').findOne(
+                { enemy: player.current_enemy }
+            )
+            
+            //making a random index to choose enemy skill
+            let randomEnemySkillIndex = Math.floor(Math.random() * enemy_current.skill.length)
+
+            enemy_new_skill = enemy_current.skill[randomEnemySkillIndex]
+
+            let enemy_change_skill = await client.db('ds_db').collection('stats').updateOne(
+                {playerId : player.playerId},
+                { $set: { enemy_next_move: enemy_new_skill } }                
+            )
+
+            res.send(`You did 2 damage and the ${player.current_enemy} did ${player.enemy_next_move.damage} damage!`)
 
         } else {
-            res.send("Cannot attack")
+            res.send("Not enough attack action!")
         }
 
     } else if(req.body.action == "evade") {
 
         if(player.evade_action > 0) {
 
-            res.send("evade")
-
             let result = await client.db('ds_db').collection('stats').updateOne(    //evade: does not take damage
                 {playerId : player.playerId},                                       //so only reduce evade points
                 { $inc: {evade_action: -1} }
             )
 
-            //TODO
-            //randomise next enemy_action
+            //randomise enemy next action
+            let enemy_current = await client.db('ds_db').collection('almanac').findOne(
+                { enemy: player.current_enemy }
+            )
+            
+            //making a random index to choose enemy skill
+            let randomEnemySkillIndex = Math.floor(Math.random() * enemy_current.skill.length)
+
+            enemy_new_skill = enemy_current.skill[randomEnemySkillIndex]
+
+            let enemy_change_skill = await client.db('ds_db').collection('stats').updateOne(
+                {playerId : player.playerId},
+                { $set: { enemy_next_move: enemy_new_skill } }                
+            )
+
+            res.send(`The ${player.current_enemy} tried to hit with ${player.enemy_next_move.damage} damage but you dodged it!`)
 
         } else {
-            res.send("Cannot evade")
+            res.send("Not enough evade action!")
         }
     } else if(req.body.action == "defend") { 
-        
-        res.send("defend")
 
-        let enemy_next_action = player.enemy_next_move  //get enemy's next move from stats
-        let enemy_almanac = await client.db('ds_db').collection('almanac').findOne({skill:{$elemMatch:{attack_name:enemy_next_action}}})    //find info from almanac
-
-        let enemy_skill = enemy_almanac.skill.find(skill => skill.attack_name === enemy_next_action);   //get the json corresponding to its attack_name
-
-        console.log(enemy_skill.damage);
-        console.log(enemy_skill);
-
-        let half_damage = Math.ceil(enemy_skill.damage / 2); // Calculate half damage, rounding up if necessary
+        let half_damage = Math.ceil(player.enemy_next_move.damage / 2); // Calculate half damage, rounding up if necessary
 
         let result = await client.db('ds_db').collection('stats').updateOne(
             { playerId: player.playerId },
             { $inc: { health_pts: -half_damage } }
         )
+
+        //Find the reference of the enemy to choose the set of skills available
+        let currentEnemy = await client.db('ds_db').collection('almanac').findOne(
+            {
+                enemy: player.current_enemy
+            }
+        )
+
+        //making a random index to choose enemy skill
+        let randomEnemySkillIndex = Math.floor(Math.random() * currentEnemy.skill.length)
+
+        //this is the skill of the enemy chosen at random
+        let chosenEnemySkill = currentEnemy.skill[randomEnemySkillIndex]
+
+        //update it with a new randomised enemy
+        let new_enemy = await client.db('ds_db').collection('stats').updateOne(
+            { playerId: player.playerId },
+            {
+                $set:
+                {
+                    enemy_next_move: chosenEnemySkill
+                }
+            }
+        )
+
+        res.send(`Defended! You only took ${half_damage} damage!`)
 
     } else {
         res.send("Invalid Action")
@@ -114,42 +170,44 @@ Next_Action_Router.get('/next_action', async (req, res) => {    // player give t
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Next_Action_Router.post('/register',async(req,res)=>{
-//     let Exists= await client.db("ds_db").collection("account").findOne({
-//         player:req.body.player
-//     });
-//     if(Exists){
-//         res.status(404).send("Player already exists");
-//     }
-//     else{
-//         const hash = bcrypt.hashSync(req.body.password, 10);
-//         let result= await client.db("ds_db").collection("account").insertOne({
-//             player:req.body.player,
-//             password:hash
-//         });
-//         let document = result1[0]; // get the first document from the result array
-//         let skills = document.skill;
+Next_Action_Router.post('/register',async(req,res)=>{
+    let Exists= await client.db("ds_db").collection("account").findOne({
+        player:req.body.player
+    });
+    if(Exists){
+        res.status(404).send("Player already exists");
+    }
+    else{
+        const hash = bcrypt.hashSync(req.body.password, 10);
+        let result= await client.db("ds_db").collection("account").insertOne({
+            player:req.body.player,
+            password:hash
+        });
 
-//         // Generate a random index
-//         let randomIndex = Math.floor(Math.random() * skills.length);
+        let result1 = await client.db('ds_db').collection('almanac').aggregate([{$sample:{size:1}}]).toArray();
 
-//         // Get a random skill
-//         let randomSkill = skills[randomIndex];
+        let document = result1[0]; // get the first document from the result array
+        let skills = document.skill;
+
+        // Generate a random index
+        let randomIndex = Math.floor(Math.random() * skills.length);
+
+        // Get a random skill
+        let randomSkill = skills[randomIndex];
 
 
         
 
-//         let statPlayer= await client.db("ds_db").collection("stats").insertOne({
-//             playerID:req.body.player,
-//             inventory:0,
-//             attack_action:10,
-//             current_enemy:document.enemy,
-//             current_score:0,
-//             enemy_health:document.base_health,
-//             enemy_next_move:randomSkill.attack_name,
-//             evade_acrion:5,
-//             heath_pts:10
-      
-//        })
-//     res.send({message:"Account created successfully, please remember your player id"});
-// }})
+        let statPlayer= await client.db("ds_db").collection("stats").insertOne({
+            playerID:req.body.player,
+            heath_pts:10,
+            attack_action:10,
+            evade_action:5,
+            inventory:0,
+            current_enemy:document.enemy,
+            enemy_health:document.base_health,
+            enemy_next_move:randomSkill,
+            current_score:0
+       })
+    res.send({message:"Account created successfully, please remember your player id"});
+}})
