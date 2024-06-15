41c506b2f6679224ff274812a41d46dbdce90f06
Khairul Ikhwan <138441635+KLimaLima@users.noreply.github.com>
2024-06-15 21:53:48 +0800

    Next move KHAI FINISH (HOPEFULLY) (#15)
    * create functions
    
    * clean up
    
    * updated enemy after player action
    
    * fixed bug
    
    * enemy reduce player health
    
    * Finally working
    
    * delete old code

--- next_action.js
+++ next_action.js
@@ -1,10 +1,19 @@
-const bcrypt = require('bcrypt')
 const express = require('express');
 const Action_Router = express.Router();
 module.exports = Action_Router;
 
-let client = require(`./database.js`)
+const bcrypt = require('bcrypt') //DELETE LATER
 
+let client = require(`./database.js`)
+let ds_db = client.db('ds_db')
+let collection_action = ds_db.collection('action')
+let collection_stats = ds_db.collection('stats')
+
+let { getPlayerStats } = require(`./valid.js`)
+
+let { update_enemy } = require(`./update_enemy.js`)
+
+//FINISH
 Action_Router.post('/action', async (req, res) => {
 
     let playerId = req.body.playerId
@@ -16,20 +25,17 @@ Action_Router.post('/action', async (req, res) => {
         return
     }
 
-    //Validate! Check if the player exists
-    let playerExist = await client.db('ds_db').collection('stats').findOne(
-        {playerId: playerId}
-    )
+    //get player data
+    let player = await getPlayerStats(playerId, res)
 
-    //If player does not exist, then reject
-    if(!playerExist) {
-        res.send(`Could not find ${playerId}.`)
+    //reject if no player data
+    if (!player) {
         return
     }
 
     //Validate! Check if player already have an action
-    let playerAction = await client.db('ds_db').collection('action').findOne(
-        {playerId: playerId}
+    let playerAction = await collection_action.findOne(
+        { playerId: playerId }
     )
 
     //If they have an active action, then reject
@@ -45,23 +51,19 @@ Action_Router.post('/action', async (req, res) => {
     }
 
     //add the action
-    let addAction = await client.db('ds_db').collection('action').insertOne(
+    let addAction = await collection_action.insertOne(
         {
             playerId: playerId,
             action: action
         }
     )
 
-    //just to show the action that has been inserted
-    let currentAction = await client.db('ds_db').collection('action').findOne(
-        {
-            playerId: playerId
-        }
-    )
+    let currentAction = await getActiveAction(playerId, res)
 
     res.send(`You've added an action:\n${currentAction.action}`)
 })
 
+//FINISH
 Action_Router.get('/action', async (req, res) => {
 
     let playerId = req.body.playerId
@@ -72,204 +74,118 @@ Action_Router.get('/action', async (req, res) => {
         return
     }
 
-    //Validate! Check if the player exists
-    let playerExist = await client.db('ds_db').collection('stats').findOne(
-        {playerId: playerId}
-    )
+    let player = await getPlayerStats(playerId, res)
 
-    //If player does not exist, then reject
-    if(!playerExist) {
-        res.send(`Could not find ${playerId}.`)
+    if (!player) {
         return
     }
 
-    let playerData = await client.db('ds_db').collection('action').findOne(
-        {
-            playerId: playerId
-        }
-    )
+    let playerAction = await getActiveAction(playerId, res)
 
-    if(!playerData) {
-        res.send('No active action found')
+    if (!playerAction) {
         return
     }
 
-    res.send(playerData)
+    res.send(playerAction)
 })
 
-//TODO redo the code
-Action_Router.patch('/end_turn', async (req, res) => {    // player give the next action they want to do
+//FINISH
+Action_Router.patch('/action', async (req, res) => {
 
-    //Validate! Check if there is enough data?
-    if (!req.body.action || !req.body.playerId) {    //data to be checked availability
-        res.send(`There's some undefined field.\nplayerId: ${req.body.playerId}\naction: ${req.body.action}`)
-        return
-    }
-    //console.log("This is in req body", req.body)
+    let playerId = req.body.playerId
 
-    let player = await client.db('ds_db').collection('stats').findOne(  //find a document by playerId referring to playerId
-        {
-            playerId: req.body.playerId
-        }
-    )
+    //get player data
+    let player = await getPlayerStats(playerId, res)
 
-    console.log(player)
-
-    if (!player) {           //if there is no player in the database(stats); reject
-        res.send('Could not find your player')
+    //reject if no player data
+    if (!player) {
         return
     }
 
-    if (req.body.action == "attack") {       //if player choose to attack
+    let deleted_action = await deleteAction(playerId, res)
 
-        //console.log(player)
+    if (!deleted_action) {
+        return      //function already res message
+    }
 
-        if (player.attack_action <= 0) {  //attack action available?
-            res.send("Not enough attack action!")
-            return
-        }
+    if (deleted_action.action == "attack" && player.attack_action > 0) {
 
-        let after_player_action = await client.db('ds_db').collection('stats').updateOne(   //damage to enemy and consume attack action
-            { playerId: player.playerId },
-            { $inc: { enemy_current_health: -2, attack_action: -1 } }
-        )
-
-        //check if enemy health <= 0 //refer to 2 as it must not be less than zero
-        if (player.enemy_current_health <= 2) {
-
-            let enemy_list = await client.db('ds_db').collection('almanac').find().toArray()
-
-            //making a random index to choose in almanac
-            let randomEnemyIndex = Math.floor(Math.random() * enemy_list.length)
-
-            //this is the enemy chosen at random
-            let chosenEnemy = enemy_list[randomEnemyIndex]
-
-            //making a random index to choose enemy skill
-            let randomEnemySkillIndex = Math.floor(Math.random() * chosenEnemy.skill.length)
-
-            //this is the skill of the enemy chosen at random
-            let chosenEnemySkill = chosenEnemy.skill[randomEnemySkillIndex]
-
-            //update it with a new randomised enemy
-            let new_enemy = await client.db('ds_db').collection('stats').updateOne(
-                { playerId: player.playerId },
-                {
-                    $set:
-                    {
-                        current_enemy: chosenEnemy.enemy,
-                        enemy_current_health: chosenEnemy.base_health,
-                        enemy_next_move: chosenEnemySkill
-                    }
+        await collection_stats.updateOne(
+            { playerId: deleted_action.playerId },
+            {
+                $inc: {
+                    enemy_current_health: -2,
+                    attack_action: -1,
+                    health_pts: (-1 * player.enemy_next_move.damage)
                 }
+            }
+        )
+
+        //Process player health = 0 AND DELETE STATS
+        let isAlive = await isPlayerAlive(playerId, res)
+        if (!isAlive) {
+
+            let deletePlayer = await collection_stats.deleteOne(
+                { playerId: playerId }
             )
-
-            res.send(`The ${player.current_enemy} died\nA ${chosenEnemy.enemy} appeared!`)
-
-            //don't continue code (use return)
             return
         }
 
-        let enemy_action_damage = player.enemy_next_move.damage  //get enemy_next_move damage from stats(player's stats)
+        //process enemy setup if not dead
+        await update_enemy(playerId)
 
-        console.log(player.enemy_next_move);
-        //console.log(enemy_action_damage);
-
-        let after_enemy_action = await client.db('ds_db').collection('stats').updateOne(    //enemy damage the player based on its next_attack
-            { playerId: player.playerId },
-            { $inc: { health_pts: (-1 * enemy_action_damage) } }
+        //just to show data to player
+        let latest_stats = await collection_stats.findOne(
+            { playerId: playerId }
         )
+        res.send(`Player Health: ${latest_stats.health_pts}\nEnemy Health: ${latest_stats.enemy_current_health}\nEnemy Next Action: ${latest_stats.enemy_next_move.attack_name}`)
 
-        //randomise enemy next action
-        let enemy_current = await client.db('ds_db').collection('almanac').findOne(
-            { enemy: player.current_enemy }
-        )
+    } else if (deleted_action.action == "evade" && player.evade_action > 0) {
 
-        //making a random index to choose enemy skill
-        let randomEnemySkillIndex = Math.floor(Math.random() * enemy_current.skill.length)
-
-        enemy_new_skill = enemy_current.skill[randomEnemySkillIndex]
-
-        let enemy_change_skill = await client.db('ds_db').collection('stats').updateOne(
-            { playerId: player.playerId },
-            { $set: { enemy_next_move: enemy_new_skill } }
-        )
-
-        //TODO take account player health if it ia zero
-
-        res.send(`You did 2 damage and the ${player.current_enemy} did ${player.enemy_next_move.damage} damage!`)
-
-    } else if (req.body.action == "evade") {
-
-        if (player.evade_action <= 0) {
-            res.send("Not enough evade action!")
-            return
-        }
-
-        let result = await client.db('ds_db').collection('stats').updateOne(    //evade: does not take damage
-            { playerId: player.playerId },                                       //so only reduce evade points
+        await collection_stats.updateOne(
+            { playerId: deleted_action.playerId },
             { $inc: { evade_action: -1 } }
         )
 
-        //randomise enemy next action
-        let enemy_current = await client.db('ds_db').collection('almanac').findOne(
-            { enemy: player.current_enemy }
-        )
+        //since evade, player will not get hit and enemy will change move; enemy also do not receive any damage
+        res.send(`You evaded the enemy's ${player.enemy_next_move.attack_name}`)
 
-        //making a random index to choose enemy skill
-        let randomEnemySkillIndex = Math.floor(Math.random() * enemy_current.skill.length)
+        //process enemy setup
+        await update_enemy(playerId)
 
-        enemy_new_skill = enemy_current.skill[randomEnemySkillIndex]
+    } else if (deleted_action.action == "defend") {
 
-        let enemy_change_skill = await client.db('ds_db').collection('stats').updateOne(
-            { playerId: player.playerId },
-            { $set: { enemy_next_move: enemy_new_skill } }
-        )
-
-        res.send(`The ${player.current_enemy} tried to hit with ${player.enemy_next_move.damage} damage but you dodged it!`)
-
-    } else if (req.body.action == "defend") {
-
-        let half_damage = Math.ceil(player.enemy_next_move.damage / 2); // Calculate half damage, rounding up if necessary
-
-        let result = await client.db('ds_db').collection('stats').updateOne(
+        // Calculate half damage, rounding up if necessary
+        let half_damage = Math.ceil(player.enemy_next_move.damage / 2);
+        let result = await collection_stats.updateOne(
             { playerId: player.playerId },
             { $inc: { health_pts: -half_damage } }
         )
 
-        //Find the reference of the enemy to choose the set of skills available
-        let currentEnemy = await client.db('ds_db').collection('almanac').findOne(
-            {
-                enemy: player.current_enemy
-            }
+        //Process if player health = 0 AND DELETE STATS
+        let isAlive = await isPlayerAlive(playerId, res)
+        if (!isAlive) {
+
+            let deletePlayer = await collection_stats.deleteOne(
+                { playerId: playerId }
+            )
+        }
+
+        //enemy setup
+        await update_enemy(playerId)
+
+        //just to show player data
+        let latest_stats = await collection_stats.findOne(
+            { playerId: playerId }
         )
+        res.send(`Player Health: ${latest_stats.health_pts}\nEnemy Health: ${latest_stats.enemy_current_health}\nEnemy Next Action: ${latest_stats.enemy_next_move.attack_name}`)
 
-        //making a random index to choose enemy skill
-        let randomEnemySkillIndex = Math.floor(Math.random() * currentEnemy.skill.length)
 
-        //this is the skill of the enemy chosen at random
-        let chosenEnemySkill = currentEnemy.skill[randomEnemySkillIndex]
+    } else { res.send('Unable to do action,\nYou can choose "attack", "evade" and "defend"\nYou need enough action points to use "attack" and "evade"') }
 
-        //update it with a new randomised enemy
-        let new_enemy = await client.db('ds_db').collection('stats').updateOne(
-            { playerId: player.playerId },
-            {
-                $set:
-                {
-                    enemy_next_move: chosenEnemySkill
-                }
-            }
-        )
-
-        //TODO Take account if player health
-
-        res.send(`Defended! You only took ${half_damage} damage!`)
-
-    } else {
-        res.send("Invalid Action")
-    }
 })
 
+//FINISH
 Action_Router.delete('/action', async (req, res) => {
 
     let playerId = req.body.playerId
@@ -280,37 +196,69 @@ Action_Router.delete('/action', async (req, res) => {
         return
     }
 
-    //Validate! Check if the player exists
-    let playerExist = await client.db('ds_db').collection('stats').findOne(
-        {playerId: playerId}
-    )
+    //find player in stats
+    let player = await getPlayerStats(playerId, res)
 
-    //If player does not exist, then reject
-    if(!playerExist) {
-        res.send(`Could not find ${playerId}.`)
+    if (!player) {
         return
     }
 
+    //delete player's action
+    let deleted_action = await deleteAction(playerId, res)
+
+    //send message; must do this because the function could be sending res when not found player action
+    if (deleted_action) {
+        console.log(deleted_action)
+        res.send(`You've deleted your active action`)
+    }
+})
+
+async function getActiveAction(playerId, res) {
+
     //Validate! Check if player have an active action
-    let playerAction = await client.db('ds_db').collection('action').findOne(
-        {playerId: playerId}
+    let playerAction = await collection_action.findOne(
+        { playerId: playerId }
     )
 
     //If there is no active action, then reject
     if (!playerAction) {
         res.send(`No active action found`)
-        return
+        return false
     }
 
-    //delete the action
-    let deleteAction = await client.db('ds_db').collection('action').deleteOne(
+    return playerAction
+}
+
+async function deleteAction(playerId, res) {
+
+    let active_action = await getActiveAction(playerId, res)
+
+    if (!active_action) {
+        return false
+    }
+
+    let deleteAction = await collection_action.deleteOne(
         {
-            playerId: playerId,
+            _id: active_action._id
         }
     )
 
-    res.send(`You've deleted your active action`)
-})
+    return active_action
+}
+
+async function isPlayerAlive(playerId, res) {
+
+    let player = await collection_stats.findOne(
+        { playerId: playerId }
+    )
+
+    if (player.health_pts <= 0) {
+        res.send("You Died")
+    }
+
+    return player.health_pts > 0
+
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
