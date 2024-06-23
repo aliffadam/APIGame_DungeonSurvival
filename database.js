48bcc3368f9c86c9f779cbb891012d02a799660a
Khairul Ikhwan <138441635+KLimaLima@users.noreply.github.com>
2024-06-23 22:51:12 +0800

    hacker go away shuuu shuuu (#40)

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
+require("dotenv").config()
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ds_dev:ds_devgroupb@clusterds.imsywsc.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDS";
+const uri = `${process.env.DBURI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
